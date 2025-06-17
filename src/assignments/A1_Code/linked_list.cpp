d59fe6092bc9c7df7997e2a3487d29ac69cb9ca6
SAT-oO <82421061+SAT-oO@users.noreply.github.com>
2025-06-17 23:38:39 +0000

    nicer clang format, credit to jason x

--- src/assignments/A1_Code/linked_list.cpp
+++ src/assignments/A1_Code/linked_list.cpp
@@ -5,76 +5,69 @@ using namespace std;
 
 class Node {
 public:
-    int   data;
-    Node *next;
+	int data;
+	Node *next;
 
-    Node( int val )
-    {
-        data = val;
-        next = nullptr;
-    }
+	Node(int val) {
+		data = val;
+		next = nullptr;
+	}
 };
 
 class LinkedList {
 public:
-    Node *head;
+	Node *head;
 
-    LinkedList() { head = nullptr; }
+	LinkedList() {
+		head = nullptr;
+	}
 
-    void insert( int val );
+	void insert(int val);
 
-    vector<int> display();
+	vector<int> display();
 
-    void reverseKGroup( int k );
+	void reverseKGroup(int k);
 
-    bool detectLoop();
+	bool detectLoop();
 
-    void reorderOddEven();
+	void reorderOddEven();
 };
 
-vector<int> LinkedList ::display()
-{
-    // Return the elements of the linked list as a vector
-    if( head == nullptr )
-    {
-        return {};
-    }
+vector<int> LinkedList ::display() {
+	// Return the elements of the linked list as a vector
+	if (head == nullptr) {
+		return {};
+	}
 
-    Node       *curr = head;
-    vector<int> vec;
-    while( curr != nullptr )
-    {
-        vec.push_back( curr->data );
-        curr = curr->next;
-    }
-    return vec;
+	Node *curr = head;
+	vector<int> vec;
+	while (curr != nullptr) {
+		vec.push_back(curr->data);
+		curr = curr->next;
+	}
+	return vec;
 }
-void LinkedList::insert( int val )
-{
+void LinkedList::insert(int val) {
+	// Insert a new node with the given value at the end of the list
+	Node *new_node = new Node(val);
+	// if head is null
+	if (head == nullptr) {
+		head = new_node;
+		return;
+	}
+	// if list isnt null
+	Node *curr = head;
+	while (curr->next != nullptr) {
+		curr = curr->next;
+	} // on the last one
 
-    // Insert a new node with the given value at the end of the list
-    Node *new_node = new Node( val );
-    // if head is null
-    if( head == nullptr )
-    {
-        head = new_node;
-        return;
-    }
-    // if list isnt null
-    Node *curr = head;
-    while( curr->next != nullptr )
-    {
-        curr = curr->next;
-    }    // on the last one
-
-    curr->next = new_node;
+	curr->next = new_node;
 }
 
 // insert node between curr and curr->next
-void insert( Node *insert, Node *curr )
-{
-    insert->next = curr->next;
-    curr->next   = insert;
+void insert(Node *insert, Node *curr) {
+	insert->next = curr->next;
+	curr->next = insert;
 }
 
 /**
@@ -89,111 +82,96 @@ void insert( Node *insert, Node *curr )
  *  temp_next (to anchor back to),curr_end (insert after)
  * 3. iterate k-1 times
  */
-void LinkedList::reverseKGroup( int k )
-{
+void LinkedList::reverseKGroup(int k) {
+	Node *prev_end = head;
+	Node *temp_next = head;
+	Node *curr_end = head;
+	Node *temp = head;
 
-    Node *prev_end  = head;
-    Node *temp_next = head;
-    Node *curr_end  = head;
-    Node *temp      = head;
+	while (true) {
+		// 1. check group availability
+		int count = k - 1;
 
-    while( true )
-    {
+		while (curr_end != nullptr && count > 0) {
+			curr_end = curr_end->next;
+			count--;
+		}
+		if (curr_end == nullptr) {
+			return; // exited while done
+					// [curr_start ... curr_end]
+		}
 
-        // 1. check group availability
-        int count = k - 1;
+		// 2. iterate k-1 times to insert, update head for once
+		if (prev_end == head) {
+			head = curr_end; // update list (iterated once)
+		} else {
+			prev_end->next = curr_end;
+			prev_end = temp_next;
+		}
 
-        while( curr_end != nullptr && count > 0 )
-        {
-            curr_end = curr_end->next;
-            count--;
-        }
-        if( curr_end == nullptr )
-            return;    // exited while done
-                       // [curr_start ... curr_end]
+		while (temp_next != curr_end) {
+			// move on for temp
+			temp = temp_next;
+			temp_next = temp->next;
 
-        // 2. iterate k-1 times to insert, update head for once
-        if( prev_end == head )
-        {
-            head = curr_end;    // update list (iterated once)
-        }
-        else
-        {
-            prev_end->next = curr_end;
-            prev_end       = temp_next;
-        }
+			// insert temp after curr_end
+			temp->next = curr_end->next;
+			curr_end->next = temp;
+		}
 
-        while( temp_next != curr_end )
-        {
-            // move on for temp
-            temp      = temp_next;
-            temp_next = temp->next;
+		// end of cycle for this group, maintain variables
 
-            // insert temp after curr_end
-            temp->next     = curr_end->next;
-            curr_end->next = temp;
-        }
-
-        // end of cycle for this group, maintain variables
-
-        count     = k - 1;
-        temp      = prev_end->next;
-        temp_next = prev_end->next;
-        curr_end  = prev_end->next;
-    }
+		count = k - 1;
+		temp = prev_end->next;
+		temp_next = prev_end->next;
+		curr_end = prev_end->next;
+	}
 }
 
-bool LinkedList::detectLoop()
-{
-    // Return true if a cycle is found in the list
+bool LinkedList::detectLoop() {
+	// Return true if a cycle is found in the list
 
-    if( head == nullptr )
-    {
-        return false;
-    }
+	if (head == nullptr) {
+		return false;
+	}
 
-    if( head->next == head )
-    {
-        return true;
-    }
+	if (head->next == head) {
+		return true;
+	}
 
-    Node *slow = head;
-    Node *fast = head;
+	Node *slow = head;
+	Node *fast = head;
 
-    // will be stuck in here is cycle
-    while( ( slow->next != nullptr ) && ( fast->next->next != nullptr ) )
-    {
+	// will be stuck in here is cycle
+	while ((slow->next != nullptr) && (fast->next->next != nullptr)) {
+		slow = slow->next;
+		fast = fast->next->next;
 
-        slow = slow->next;
-        fast = fast->next->next;
+		if (slow->data == fast->data) {
+			return true;
+		}
+	}
 
-        if( slow->data == fast->data )
-        {
-            return true;
-        }
-    }
-
-    return false;
+	return false;
 }
 
-void LinkedList::reorderOddEven()
-{
-    // Reorder the list with odd-positioned nodes before even-positioned ones
-    if( ( head == nullptr ) || ( head->next == nullptr ) )
-        return;
+void LinkedList::reorderOddEven() {
+	// Reorder the list with odd-positioned nodes before even-positioned ones
+	if ((head == nullptr) || (head->next == nullptr)) {
+		return;
+	}
 
-    Node *even_head = head->next;
+	Node *even_head = head->next;
 
-    Node *curr1 = head;
-    Node *curr2 = head->next;
+	Node *curr1 = head;
+	Node *curr2 = head->next;
 
-    while( ( curr1->next != nullptr ) && ( curr2->next != nullptr ) )
-    {
-        curr1->next = curr2->next;
-        curr1       = curr1->next;
-        curr2->next = curr1->next;
-        curr2       = curr2->next;
-    }
+	while ((curr1->next != nullptr) && (curr2->next != nullptr)) {
+		curr1->next = curr2->next;
+		curr1 = curr1->next;
+		curr2->next = curr1->next;
+		curr2 = curr2->next;
+	}
 
-    curr1->next = even_head;
+	curr1->next = even_head;
 }
